233a1206ca98a988cdbe2aca0f4e90c1eb755cce
shinythinking <shinythink@gmail.com>
2025-11-10 17:09:24 +0900

    chore: test를 위한 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,15 +28,13 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
+    }
+
+    kotlin {
+        jvmToolchain(17)
     }
 }
 
@@ -53,8 +51,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    // Coroutine
+    implementation(libs.kotlinx.coroutines)
+
     // Hilt
     implementation(libs.hilt.android)
+    implementation(libs.androidx.junit.ktx)
     ksp(libs.hilt.compiler)
     testImplementation(libs.hilt.testing)
     kspTest(libs.hilt.compiler)
@@ -66,14 +68,17 @@ dependencies {
     implementation(libs.room.runtime)
     ksp(libs.room.compiler)
     implementation(libs.room.ktx)
-    testImplementation(libs.room.testing)
+    androidTestImplementation(libs.room.testing)
 
-    // Testing (Unit & Instrumentation)
+    // Testing
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.androidx.test.core.ktx)
 
     // Debugging Tools
     debugImplementation(libs.androidx.compose.ui.tooling)
