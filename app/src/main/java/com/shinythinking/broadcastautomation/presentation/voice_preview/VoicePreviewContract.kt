040a5d2a93cc034b9842337dc53974b66a8ed2cc
shinythinking <shinythink@gmail.com>
2025-11-21 16:56:26 +0900

    feat: VoicePreviewContract 설정
    * uiState

---
+++ app/src/main/java/com/shinythinking/broadcastautomation/presentation/voice_preview/VoicePreviewContract.kt
@@ -0,0 +1,19 @@
+package com.shinythinking.broadcastautomation.presentation.voice_preview
+
+sealed interface VoicePreviewUiState {
+    data class Idle(
+        val progress: Float = 0f
+    ) : VoicePreviewUiState
+
+    data class Playing(
+        val progress: Float
+    ) : VoicePreviewUiState
+
+    data class Paused(
+        val progress: Float
+    ) : VoicePreviewUiState
+
+    data class Completed(
+        val duration: Float = 1f
+    ) : VoicePreviewUiState
+}
