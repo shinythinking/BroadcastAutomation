8d3cb1604e3ceafed9f1e8b4cb97588a50ada5c1
shinythinking <shinythink@gmail.com>
2025-12-16 17:13:45 +0900

    feat: BroadcastRepository 구현
    * cancel은 todo로 이후에 구현

--- app/src/main/java/com/shinythinking/broadcastautomation/domain/repository/BroadcastRepository.kt
+++ app/src/main/java/com/shinythinking/broadcastautomation/domain/repository/BroadcastRepository.kt
@@ -1,19 +1,8 @@
 package com.shinythinking.broadcastautomation.domain.repository
 
-import com.shinythinking.broadcastautomation.domain.model.Script
-import com.shinythinking.broadcastautomation.domain.model.Template
-import kotlinx.coroutines.flow.Flow
+import com.shinythinking.broadcastautomation.domain.model.BroadcastMessage
 
 interface BroadcastRepository {
-    fun getTemplatesFlow(): Flow<List<Template>>
-    suspend fun getTemplate(templateId: String): Template?
-    suspend fun saveTemplate(template: Template)
-    suspend fun deleteTemplate(templateId: String)
-
-    fun getScriptsFlow(): Flow<List<Script>>
-    suspend fun getScript(scriptId: String): Script?
-    fun searchScripts(query: String): Flow<List<Script>>
-    suspend fun saveScript(script: Script): String
-    suspend fun deleteScript(scriptId: String)
-    suspend fun updateFavoriteStatus(scriptId: String, isFavorite: Boolean)
+    suspend fun sendBroadcast(message: BroadcastMessage): Result<Unit>
+    suspend fun cancelScheduledBroadcast(broadcastId: String)
 }
