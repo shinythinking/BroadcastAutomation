1a2b7154dc513a8fcc4c1af179318c58aa01a9ab
shinythinking <shinythink@gmail.com>
2025-12-16 20:01:34 +0900

    feat: Ktor 관련 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)  // 추가 - 중요!
+    alias(libs.plugins.room)
 }
 
 android {
@@ -38,6 +41,10 @@ android {
     }
 }
 
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
 dependencies {
     // AndroidX Core & Lifecycle
     implementation(libs.androidx.core.ktx)
@@ -69,6 +76,14 @@ dependencies {
     ksp(libs.room.compiler)
     implementation(libs.room.ktx)
     androidTestImplementation(libs.room.testing)
+
+    // ktor
+    implementation(platform(libs.ktor.bom))
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.logging)
 
     // Testing
     testImplementation(libs.junit)
