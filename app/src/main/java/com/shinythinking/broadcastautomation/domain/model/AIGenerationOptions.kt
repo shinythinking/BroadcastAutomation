d25351a15e9ae3b5925bb7ff9c85f8950df2bd95
shinythinking <shinythink@gmail.com>
2025-12-20 21:35:29 +0900

    feat: Gemini 사용을 위한 Dto 설정 + DataSource 설정

--- app/src/main/java/com/shinythinking/broadcastautomation/domain/model/AIGenerationOptions.kt
+++ app/src/main/java/com/shinythinking/broadcastautomation/domain/model/AIGenerationOptions.kt
@@ -6,13 +6,18 @@ import androidx.compose.runtime.Immutable
 data class AIGenerationOptions(
     val tone: AITone = AITone.NORMAL,
     val length: AILength = AILength.SHORT
-)
+) {
+    fun toPrompt(): String {
+        return "문장을 ${tone.value} ${length.description} 분량으로 작성해주세요."
+    }
+}
 
-enum class AITone(val displayName: String) {
-    NORMAL("일반적으로"),
-    URGENT("긴급하게"),
-    SOFT("부드럽게"),
-    FORMAL("격식있게")
+enum class AITone(val displayName: String, val value: String) {
+    NORMAL("일반적으로", "일반적인 톤으로"),
+    URGENT("긴급하게", "긴급한 톤으로"),
+    SOFT("부드럽게", "부드러운 톤으로"),
+    FORMAL("격식있게", "격식있는 톤으로"),
+    Wit("재치 있게", "재치 있는 톤으로")
 }
 
 enum class AILength(val displayName: String, val description: String) {
